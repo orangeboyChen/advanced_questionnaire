47b099c5d8141d765f3d057c6dca93f5869d9b17
orangeboy <chenenhan@LIVE.COM>
2020-04-26 22:55:03 +0800

    添加了Swagger，初始化项目结构

---
+++ questionnaire-service-provider/src/main/java/com/wxapp/questionnaire/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.wxapp.questionnaire.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author orangeboy
+ * 获取用户信息的Controller
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
